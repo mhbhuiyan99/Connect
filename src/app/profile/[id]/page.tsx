f91dbdd4c0dc2fd8dac3f6c378368551f895b8c9
HImel Hasnat <himelhasnatc41@gmail.com>
2025-05-08 23:42:36 +0600

    user info & media

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,6 +1,28 @@
+import Feed from "@/components/Feed";
+import LeftMenu from "@/components/LeftMenu";
+import RightMenu from "@/components/RightMenu";
+
+
 const Profile = () => {
     return (
-        <div className=''> Profile page </div>
+        <div className="flex gap-5">
+
+
+      <div className="hidden xl:block w-[20%]"> <LeftMenu/> </div>
+
+      <div className="w-full lg:w-[70%] xl:w-[70%]">
+
+          <div className="flex flex-col gap-5">
+            
+            <Feed/>
+          </div>
+
+      </div>
+
+      <div className="hidden lg:block w-[30%]"> <RightMenu userId="test"/> </div>
+      
+
+    </div>
     )
 }
 
--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -1,10 +1,18 @@
 import FriendRequests from "./FriendRequests";
-import Birthdays from "./BIrthdays";
-import Ad from "./Ad";
+import UserInfoCard from "./UserInfoCard";
+import UserMediaCard from "./UserMediaCard";
 
 const RightMenu = ({userId}:{userId?:string}) =>{
     return(
         <div className="flex flex-col gap-6"> 
+        {userId ? (<>
+        <UserInfoCard userId={userId}/>
+        <UserMediaCard userId={userId}/>
+
+        </>
+
+
+        ) : null }
         <FriendRequests />
         
         
