c4cff96e958242e8834cff6ceaa36603e0a5e424
Md. Hasibul Kabir <46620128+HasibulKabir@users.noreply.github.com>
2025-06-19 16:28:26 +0600

    ⚪ Use white theme

--- src/components/form/SignInForm.tsx
+++ src/components/form/SignInForm.tsx
@@ -77,7 +77,7 @@ const SignInForm = () => {
   }
 
   return (
-    <div className="h-full flex items-center justify-center bg-slate-300">
+    <div className="h-full flex items-center justify-center">
       <Card className="md:h-auto w-[80%] sm:w-[420px] p-4 sm:p-8">
         <CardHeader>
           <CardTitle className="text-center">Sign In</CardTitle>
